88ed08882012feb493c7c1a44ca330cbadc409e6
Rahma Muhammed <rahma.deabes30@gmail.com>
2025-12-21 18:57:09 +0200

    added settings config

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv()
 from routes import base
 
 app = FastAPI()
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
+from helpers.config import Settings, get_settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,9 +8,9 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def health_check():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+async def health_check(app_settings = Depends(get_settings)):
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION 
     return {
         "app_name": app_name,
         "app_version": app_version
