857a44a31d3e6cef344be436affa5d530f4263a9
rahma-muhammad <rahma.deabes30@gmail.com>
2025-12-29 01:25:18 +0200

    replace on_event with lifespan

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
 
 from helpers.config import get_settings
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def client_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
 
     app.mongo_connection = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_connection[settings.MONGODB_NAME]
 
-@app.on_event("shutdown")
-async def client_shutdown():
+    yield
     app.mongo_connection.close()
     
 app.include_router(base.base_router)
