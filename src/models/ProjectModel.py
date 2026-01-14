e5b9c19b50a1f99b517565c4e9c06ed3dd430207
rahma-muhammad <rahma.deabes30@gmail.com>
2026-01-14 02:21:16 +0200

    solve id error in chunk and project models

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -29,7 +29,7 @@ class ProjectModel(BaseDataModel):
 
     async def create_project(self, project: Project):
         record = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
-        project._id = record.inserted_id
+        project.id = record.inserted_id
         return project
     
     async def get_project_or_create_one(self, project_id):
