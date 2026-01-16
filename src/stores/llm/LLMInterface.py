6fe3c475ccc30eec0c52aceb9f272cf6c0cb8c13
rahma-muhammad <rahma.deabes30@gmail.com>
2026-01-17 00:00:16 +0200

    generate llm factory

---
+++ src/stores/llm/LLMInterface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class LLMInterface(ABC):
+
+    @abstractmethod
+    def set_generation_model(self, model_id: str):
+        """Set the generation model by its identifier."""
+        pass
+
+    @abstractmethod
+    def set_embedding_model(self, model_id: str):
+        """Set the embedding model by its identifier."""
+        pass
+
+    @abstractmethod
+    def generate_text(self, prompt: str, max_output_tokens: int, temperature: float = None):
+        """Generate text based on the given prompt."""
+        pass
+
+    @abstractmethod
+    def embed_text(self, text: str, document_type):
+        """Generate embeddings for the given text."""
+        pass
