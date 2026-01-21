0b263abff3888778d99820fb929ed84846a55944
rahma-muhammad <rahma.deabes30@gmail.com>
2026-01-21 23:58:06 +0200

    move to openai standards

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -1,19 +1,30 @@
 from ..LLMInterface import LLMInterface
+import logging
+from openai import OpenAI
 
-class OllamaProvider(LLMInterface):
+class OpenAIProvider(LLMInterface):
     def __init__(self,
+            api_key: str,
+            api_base_url: str,
             default_max_generation_tokens: int,
             default_max_input_characters: int,
             default_temperature: float):
         
+        self.api_key = api_key
+        self.api_url = api_base_url
         self.default_max_generation_tokens = default_max_generation_tokens
         self.default_max_input_characters = default_max_input_characters
         self.default_temperature = default_temperature
 
-        self.client = None
+        self.client = OpenAI(
+            api_key = self.api_key,
+            api_url = self.api_url
+        )
         self.generation_model_id = None
         self.embedding_model_id = None
         self.embedding_size = None
+
+        self.logger = logging.getLogger(__name__)
 
     def set_generation_model(self, model_id: str):
         self.generation_model_id = model_id
